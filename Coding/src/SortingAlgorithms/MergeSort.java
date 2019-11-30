ddf281d35b0e3dc5b2eb2f9c8fcccf2486e37790
Amr Eshra <amr.eshra@gmail.com>
2019-11-30 23:47:13 +0300

    changes in Sorting

---
+++ Coding/src/SortingAlgorithms/MergeSort.java
@@ -0,0 +1,49 @@
+package SortingAlgorithms;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+
+		int[] a = new int[] { 20, 35, -15, 7, 55, 1, -22 };
+		
+		mergeSort(a, 0, a.length);
+		
+		System.out.println(Arrays. toString(a));
+	}
+	
+    public static void mergeSort(int[] input, int start, int end) {
+
+        if (end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
+    
+    public static void merge(int[] input, int start, int mid, int end) {
+
+        if (input[mid - 1] <= input[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while (i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+
+
+    }
+
+}
