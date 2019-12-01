1c3edf5316232d674fcbc32f34794b659e5ffccc
Amr Eshra <amr.eshra@gmail.com>
2019-12-01 10:56:34 +0300

    MergeSort (ASC / DESC)

--- Coding/src/SortingAlgorithms/MergeSort.java
+++ Coding/src/SortingAlgorithms/MergeSort.java
@@ -7,41 +7,60 @@ public class MergeSort {
 	public static void main(String[] args) {
 
 		int[] a = new int[] { 20, 35, -15, 7, 55, 1, -22 };
-		
-		mergeSort(a, 0, a.length);
+		mergeSort(a, 0, a.length, "DESC");
 		
 		System.out.println(Arrays. toString(a));
 	}
 	
-    public static void mergeSort(int[] input, int start, int end) {
+    public static void mergeSort(int[] input, int start, int end, String order) {
 
         if (end - start < 2) {
             return;
         }
 
         int mid = (start + end) / 2;
-        mergeSort(input, start, mid);
-        mergeSort(input, mid, end);
-        merge(input, start, mid, end);
+        mergeSort(input, start, mid, order);
+        mergeSort(input, mid, end, order);
+        merge(input, start, mid, end, order);
     }
     
-    public static void merge(int[] input, int start, int mid, int end) {
+    public static void merge(int[] input, int start, int mid, int end, String order) {
 
-        if (input[mid - 1] <= input[mid]) {
-            return;
-        }
-
-        int i = start;
-        int j = mid;
-        int tempIndex = 0;
-
-        int[] temp = new int[end - start];
-        while (i < mid && j < end) {
-            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
-        }
-
-        System.arraycopy(input, i, input, start + tempIndex, mid - i);
-        System.arraycopy(temp, 0, input, start, tempIndex);
+    	if (order.equals("ASC")) {
+	        if (input[mid - 1] <= input[mid]) {
+	            return;
+	        }
+	
+	        int i = start;
+	        int j = mid;
+	        int tempIndex = 0;
+	
+	        int[] temp = new int[end - start];
+	        while (i < mid && j < end) {
+	            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+	        }
+	
+	        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+	        System.arraycopy(temp, 0, input, start, tempIndex);
+    	}
+    	
+    	else if (order.equals("DESC")) {
+	        if (input[mid - 1] > input[mid]) {
+	            return;
+	        }
+	
+	        int i = start;
+	        int j = mid;
+	        int tempIndex = 0;
+	
+	        int[] temp = new int[end - start];
+	        while (i < mid && j < end) {
+	            temp[tempIndex++] = input[i] > input[j] ? input[i++] : input[j++];
+	        }
+	
+	        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+	        System.arraycopy(temp, 0, input, start, tempIndex);
+    	}
 
 
     }
